a5309f9916293e6e061d19e10a65c4d6990632cf
Justin Obney <justinobney@gmail.com>
2017-08-15 20:28:16 -0500

    feat: implement required functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 import BookShelf from './BookShelf';
 import Search from './Search';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    books: [],
+  };
+
+  componentWillMount = () => this._loadBooks();
+
+  _loadBooks = async () => {
+    const myBooks = await BooksAPI.getAll();
+    this.setState({books: myBooks});
+  };
+
+  _updateBook = async updatedBook => {
+    const {books} = this.state;
+    const otherBook = books.filter(x => x.id !== updatedBook.id);
+
+    this.setState({
+      books: [
+        ...otherBook,
+        {
+          ...updatedBook,
+          title: `${updatedBook.title} (saving...)`,
+        },
+      ],
+    });
+
+    await BooksAPI.update(updatedBook, updatedBook.shelf);
+
+    this.setState({
+      books: [...otherBook, updatedBook],
+    });
+  };
+
+  _renderBookShelf = () =>
+    <BookShelf myBooks={this.state.books} onBookUpdated={this._updateBook} />;
+
+  _renderSearch = () =>
+    <Search myBooks={this.state.books} onBookUpdated={this._updateBook} />;
+
   render() {
     return (
       <Router>
         <div className="app">
-          <Route exact path="/" component={BookShelf} />
-          <Route path="/search" component={Search} />
+          <Route exact path="/" render={this._renderBookShelf} />
+          <Route path="/search" render={this._renderSearch} />
         </div>
       </Router>
     );
--- src/Search.js
+++ src/Search.js
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import debounce from 'lodash/debounce';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
-const Search = () =>
-  <div className="search-books">
-    <div className="search-books-bar">
-      <Link to="/" className="close-search">
-        Close
-      </Link>
-      <div className="search-books-input-wrapper">
-        {/*
-        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-        You can find these search terms here:
-        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class Search extends Component {
+  state = {
+    results: [],
+  };
 
-        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-        you don't find a specific author or title. Every search is limited by search terms.
-      */}
-        <input type="text" placeholder="Search by title or author" />
+  _onSearchChanged = event => this._searchApi(event.target.value);
+
+  _renderBook = book =>
+    <li key={book.id}>
+      <Book book={book} onBookUpdated={this.props.onBookUpdated} />
+    </li>;
+
+  _searchApi = debounce(async value => {
+    const results = await BooksAPI.search(value);
+    this.setState({results});
+  }, 500);
+
+  render() {
+    const {results} = this.state;
+    const {myBooks} = this.props;
+    const mergedBooks = results.map(result => {
+      const found = myBooks.find(book => book.id === result.id);
+      if (found) {
+        return {
+          ...result,
+          shelf: found.shelf,
+        };
+      }
+      return result;
+    });
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              autoFocus
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this._onSearchChanged}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            <ol className="books-grid">
+              {mergedBooks.map(this._renderBook)}
+            </ol>
+          </ol>
+        </div>
       </div>
-    </div>
-    <div className="search-books-results">
-      <ol className="books-grid" />
-    </div>
-  </div>;
-
+    );
+  }
+}
 export default Search;
